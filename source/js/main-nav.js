f16f360f4daa8c2a02dea32a2424a325f896f8dd
Alina-837-a <ALISHAKURITSYNA@yandex.ru>
2021-05-18 00:01:39 +0300

    Декоративные элементы

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,14 +1,21 @@
-var mainNav = document.querySelector(".main-nav");
-var mainNavToggle = document.querySelector(".main-nav__toggle");
 var pageHeader  = document.querySelector(".page-header");
+var pageHeaderToggle = document.querySelector(".page-header__toggle");
+var pageHeaderMenuBurger = document.querySelector(".page-header__menu-burger");
+var mainNav = document.querySelector(".main-nav");
 mainNav.classList.remove("main-nav--nojs"),
-mainNavToggle.addEventListener("click", function() {
-  if (mainNav.classList.contains("main-nav--closed")) {
+pageHeaderMenuBurger.classList.remove("page-header__menu-burger--nojs"),
+pageHeader.classList.remove("page-header--nojs");
+pageHeaderToggle.addEventListener("click", function() {
+  if (pageHeaderMenuBurger.classList.contains("page-header__menu-burger--closed")) {
+    pageHeaderMenuBurger.classList.remove("page-header__menu-burger--closed")
     mainNav.classList.remove("main-nav--closed");
+    pageHeaderMenuBurger.classList.add("page-header__menu-burger--opened")
     mainNav.classList.add("main-nav--opened");
     pageHeader.classList.add("page-header--opened");
   } else {
+    pageHeaderMenuBurger.classList.add("page-header__menu-burger--closed")
     mainNav.classList.add("main-nav--closed");
+    pageHeaderMenuBurger.classList.remove("page-header__menu-burger--opened")
     mainNav.classList.remove("main-nav--opened");
     pageHeader.classList.remove("page-header--opened");
   }
